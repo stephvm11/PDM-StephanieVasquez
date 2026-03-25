4ae237dcb52e7cb566a194fadf35cd145c4a31fc
stephvm11 <00056024@uca.edu.sv>
2026-03-25 12:56:04 -0600

    feat: Agregar archivos de laboratorio 2

---
+++ laboratorio 2/SAVMCUATRO/app/src/main/java/com/example/savmcuatro/MainActivity.kt
@@ -0,0 +1,135 @@
+package com.example.savmcuatro
+
+import androidx.compose.ui.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.savmcuatro.ui.theme.SAVMCUATROTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SAVMCUATROTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Listado(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Listado(modifier: Modifier = Modifier) {
+    val listado = mutableListOf<String>("")
+    val nombre: MutableState<String> = remember { mutableStateOf("") }
+    Column() {
+        Box(modifier = Modifier.fillMaxSize().weight(1f), contentAlignment = Alignment.Center) {
+            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                TextField(
+                    value = nombre.value,
+                    onValueChange = {
+                        nombre.value = it
+                    },
+                    maxLines = 1,
+                    minLines = 1,
+                    singleLine = true,
+                    placeholder = { Text("Nombre") },
+                    isError = false,
+                    keyboardActions = KeyboardActions(
+                        onNext = {},
+                        onDone = {},
+                        onGo = {},
+                        onPrevious = {},
+                        onSearch = {},
+                        onSend = {}
+                    ),
+                )
+                Button(
+                    onClick = {
+                        listado.add(nombre.toString())
+                    }
+                ) { Text(text = "Guardar") }
+            }
+
+        }
+        Box( modifier = Modifier.fillMaxSize().weight(1f), contentAlignment = Alignment.Center) {
+            Row(horizontalArrangement = Arrangement.SpaceBetween) {
+                Text(
+                    text = "Listado de nombres y posición en la lista",
+                    textAlign = TextAlign.Left,
+                    modifier = Modifier.weight(2f)
+                )
+                Button(
+                    onClick = {
+                        listado.clear()
+                    },
+                    modifier = Modifier.weight(1f)
+                ) { Text("Limpiar") }
+            }
+        }
+        Box(modifier = Modifier.fillMaxSize().weight(3f), contentAlignment = Alignment.Center){
+
+            Column( modifier = Modifier
+                .height(200.dp)
+                .padding(10.dp).background(Color.White),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center) {
+                val entries: List<String> = listado
+                LazyColumn() {
+                    itemsIndexed(entries.toList()) { index, item ->
+                        Row( modifier = Modifier.fillMaxWidth().padding(10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+                            Text(
+                                text = item
+                            )
+                            Text(
+                                text = (index + 1).toString()
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun ListadoPreview() {
+        SAVMCUATROTheme {
+            Listado()
+        }
+    }
